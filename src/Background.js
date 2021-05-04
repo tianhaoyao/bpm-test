da282e17792c42bf355191381d08f99a03866fb1
ricecustomer <tianhao.yao@mail.utoronto.ca>
2021-05-04 01:44:52 -0400

    blurring

--- src/Background.js
+++ src/Background.js
@@ -1,16 +1,25 @@
 import { useState, useImperativeHandle, forwardRef, useEffect} from "react";
+import bpm140 from './resources/140.jpg';
+import bpm160 from './resources/160.jpg';
 import bpm180 from './resources/180.jpg';
 import bpm200 from './resources/200.jpg';
 import bpm220 from './resources/220.jpg';
 import bpm240 from './resources/240.jpg';
+import bpm260 from './resources/260.jpg';
+import CrossfadeImage from 'react-crossfade-image'
 
 const Background = forwardRef((props, ref) => {
 
-    const [imageUrl, setImageUrl] = useState('');
+    const [imageUrl, setImageUrl] = useState(bpm180);
     const [range, setRange] = useState(0);
+    const style = {
+        width: '100%',
+        height: '100%',
+        filter: "blur(20px)"
+    }
 
     useEffect(() => {
-        let imageList = [bpm180, bpm200, bpm220, bpm240]
+        let imageList = [bpm160, bpm180, bpm200, bpm220, bpm240, bpm260]
         imageList.forEach((image) => {
             new Image().src = image
         });
@@ -18,28 +27,40 @@ const Background = forwardRef((props, ref) => {
 
     const update = (bpm) => {
         let url = '';
-        let thisrange = 0;
+        let thisrange = -1;
         console.log(bpm)
         switch(true) {
-            case (bpm >= 180 && bpm < 200):
+            case (bpm < 160):
+                thisrange = 0;
+                url = bpm140
+                break;
+            case (bpm >= 160 && bpm < 180):
                 thisrange = 1;
+                url = bpm160
+                break;
+            case (bpm >= 180 && bpm < 200):
+                thisrange = 2;
                 url = bpm180
                 break;
             case (bpm >= 200 && bpm < 220):
-                thisrange = 2;
+                thisrange = 3;
                 url = bpm200
                 break;
             case (bpm >= 220 && bpm < 240):
-                thisrange = 3;
+                thisrange = 4;
                 url = bpm220
                 break;
-            case (bpm >= 240):
-                thisrange = 4;
+            case (bpm >= 240&& bpm < 260):
+                thisrange = 5;
                 url = bpm240
                 break;
+            case (bpm >= 260):
+                thisrange = 6;
+                url = bpm260
+                break;
             default:
-                thisrange = 1;
-                url = bpm180
+                thisrange = -1;
+                url = bpm140
                 break;
         }
         if(thisrange === range) return
@@ -54,10 +75,16 @@ const Background = forwardRef((props, ref) => {
     })
 
     return (
-        <div style={{
-            backgroundImage: imageUrl
-        }}>
-          <img id="backgroundimg" src={imageUrl}></img>
+        <div>
+           
+            <CrossfadeImage 
+                id="backgroundimg" 
+                src={imageUrl}
+                style={style}
+               />
+            
+          
+          {/* <img id="backgroundimg" src={imageUrl}></img> */}
         </div>
     )
 })
